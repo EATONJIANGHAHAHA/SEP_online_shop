200f5046c922cc14deaf7a1d340bfcb6828c3f25
EATONJIANGHAHAHA <eatonjiangtonlei@gmail.com>
2017-09-25 17:57:56 +1000

    full access to item properties in database enabled.

--- src/java/com/uts/sep/dao/ItemDAO.java
+++ src/java/com/uts/sep/dao/ItemDAO.java
@@ -6,33 +6,65 @@
 package com.uts.sep.dao;
 
 import com.uts.sep.entity.ItemTbl;
+import java.util.List;
+
 /**
  *
  * @author lzy
  */
-public class ItemDAO extends BaseDAO<ItemTbl>{
-    
-    public void updateItemName(Integer itemId, String itemName){
+public class ItemDAO extends BaseDAO<ItemTbl> {
+
+    public void updateItemName(Integer itemId, String itemName) {
         ItemTbl usingItem = findById(itemId);
         usingItem.setItemName(itemName);
-        super.update(usingItem);
+        update(usingItem);
     }
-    
-    public void updateItemDescription(Integer itemId, String itemDescription){
+
+    public void updateItemDescription(Integer itemId, String itemDescription) {
         ItemTbl usingItem = findById(itemId);
         usingItem.setItemDescription(itemDescription);
-        super.update(usingItem);
+        update(usingItem);
     }
-    
-    public void updateItemStock(Integer itemId, int stock){
+
+    public void updateItemStock(Integer itemId, Integer stock) {
         ItemTbl usingItem = findById(itemId);
         usingItem.setStock(stock);
-        super.update(usingItem);
+        update(usingItem);
     }
-    
-    public void updateItemStatus(Integer itemId, int itemStatus){
+
+    public void updateItemStatus(Integer itemId, Integer itemStatus) {
         ItemTbl usingItem = findById(itemId);
         usingItem.setItemStatus(itemStatus);
-        super.update(usingItem);
+        update(usingItem);
+    }
+
+    public void updatePrice(Integer itemId, Double price) {
+        ItemTbl usingItem = findById(itemId);
+        usingItem.setPrice(price);
+        update(usingItem);
+    }
+
+    public boolean updateItemPicUrl(Integer itemId, String Url) {
+        ItemTbl usingItem = findById(itemId);
+        List<ItemTbl> items = getAll(ITEM_TBL);
+        boolean UrlIsUsed = false;
+        for(ItemTbl item :items){
+            if(item.getItemPicUrl().equals(Url)) {
+                UrlIsUsed = true;
+                return false;
+            }
+        }
+        if (!UrlIsUsed) {
+            usingItem.setItemPicUrl(Url);
+            update(usingItem);
+            return true;
+        }
+        else return false;
+    }
+    
+    public void updateCategory(Integer itemId, String category){
+        ItemTbl usingItem = findById(itemId);
+        usingItem.setCategory(category);
+        update(usingItem);
     }
 }
--- src/java/com/uts/sep/dao/testDAO.java
+++ src/java/com/uts/sep/dao/testDAO.java
@@ -16,20 +16,9 @@ public class testDAO {
 
     public static void main(String[] args) {
         ItemDAO dao = new ItemDAO();
-        List<ItemTbl> list = dao.getAll(BaseDAO.ITEM_TBL);
-
-        //dao.insertUser("ZheyiLu", "123456");
-        for (int i = 0; i < list.size(); i++) {
-            ItemTbl item = new ItemTbl();
-            item = list.get(i);
-            System.out.print(item.getItemName());
-            System.out.print(item.getStock());
-            System.out.print(item.getPrice());
-
-        }
-        
-//        AdminDAO dao = new AdminDAO();
-//        dao.getAll("from AdminTbl");
+        dao.updatePrice(1, 230.25);
+        ItemTbl usingItem = dao.findById(1);
+        System.out.print(usingItem.getPrice());
     }
 
 }
